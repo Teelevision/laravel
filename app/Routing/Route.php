c96be86a1837a66e61ee90376b0d954322c01fcc
Marius Neugebauer <marius@teele.eu>
2019-02-14 01:07:51 +0100

    routing and url generation

---
+++ app/Routing/Route.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Routing;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Matching\UriValidator;
+use App\Routing\Matching\PageValidator;
+
+class Route extends \Illuminate\Routing\Route
+{
+
+    public static function getValidators()
+    {
+        if (isset(static::$validators)) {
+            return static::$validators;
+        }
+
+        return static::$validators = static::replaceUriValidator(parent::getValidators());
+    }
+
+    protected static function replaceUriValidator(array $validators): array
+    {
+        foreach ($validators as $i => $validator) {
+            if (get_class($validator) === UriValidator::class) {
+                $validators[$i] = new PageValidator;
+                break;
+            }
+        }
+        return $validators;
+    }
+
+    public function bind(Request $request)
+    {
+        parent::bind($request);
+
+        /**
+         * Add all of the query parameters (except page) to the parameters.
+         * This way the model binding works with query parameters.
+         * Because we cannot have the model keys in the page parameter,
+         * as I don't know how that could work.
+         */
+        $this->parameters = array_merge(
+            $this->parameters,
+            array_except($request->query->all(), 'page')
+        );
+
+        // TODO: if the query doesn't contain an id for the model binding, no error is thrown
+        // TODO: if the query contains too many values, the route breaks
+
+        return $this;
+    }
+}
---
+++ app/Routing/UrlGenerator.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Routing;
+
+use Illuminate\Routing\Exceptions\UrlGenerationException;
+use App\Http\FrontendRequest;
+
+class UrlGenerator extends \Illuminate\Routing\UrlGenerator
+{
+
+    /**
+     * Get the URL for a given route instance.
+     *
+     * @param  \Illuminate\Routing\Route $route
+     * @param  mixed $parameters
+     * @param  bool $absolute
+     * @return string
+     *
+     * @throws \Illuminate\Routing\Exceptions\UrlGenerationException
+     */
+    protected function toRoute($route, $parameters, $absolute)
+    {
+        $route = clone $route;
+
+        if (starts_with($route->uri, 'frontend/')) {
+            /**
+             * Frontend route
+             */
+
+            /**
+             * Add the action of the route to the parameters.
+             */
+            $parameters['action'] = array_last(explode('/', $route->uri()));
+
+            /**
+             * Set uri of route.
+             * Either use the passed uri parameter or use the original request uri.
+             */
+            if (isset($parameters['uri'])) {
+                $route->setUri($parameters['uri']);
+                unset($parameters['uri']);
+            } elseif ($this->request instanceof FrontendRequest) {
+                $route->setUri($this->request->path());
+            } else {
+                /**
+                 * When trying to generate a frontend route from the backend and no uri parameter is given.
+                 */
+                throw UrlGenerationException::forMissingParameters($route);
+            }
+
+        } else {
+            /**
+             * Backend route (web, ..., basically everything that isn't frontend)
+             */
+
+            /**
+             * Set the uri as the page parameter.
+             */
+            $parameters['page'] = $route->uri();
+
+            if (\rex::isBackend()) {
+                /**
+                 * Generating the backend url from the backend: no uri needed.
+                 */
+                $route->setUri('');
+            } else {
+                /**
+                 * Generating the backend url from the frontend: set the uri to redaxo's backend controller.
+                 */
+                $route->setUri(\rex_url::backendController());
+            }
+        }
+
+        $uri = parent::toRoute($route, $parameters, $absolute);
+
+        return $this->fixSlashBeforeQuery($uri);
+    }
+
+
+    /**
+     * Fix missing "/" before the query part of an uri,
+     * unless it's already there or a php script is called directly.
+     *
+     * @param string $uri
+     * @return string
+     */
+    protected function fixSlashBeforeQuery(string $uri): string
+    {
+        if (str_contains($uri, '?') && !str_contains($uri, ['/?', '.php?'])) {
+            return str_replace_first('?', '/?', $uri);
+        }
+        return $uri;
+    }
+}
