e713050da0f39f13f918f1c597dc018c745350fc
Marius Neugebauer <marius@teele.eu>
2019-02-15 17:47:52 +0100

    catch POST requests that actually are just users editing Redaxo article slices

--- lib/MyLaravelAddOn.php
+++ lib/MyLaravelAddOn.php
@@ -24,6 +24,22 @@ abstract class MyLaravelAddOn
 
     protected static function handle(string $uri, array $data, rex_addon $redaxoAddOn)
     {
+        /**
+         * If you are editing a Redaxo article slice Redaxo will save all values
+         * and call the used module output code during the same request.
+         * The output is important for displaying the changes in the user interface.
+         * This means that this method can be called during such a POST request,
+         * in which case we do a redirect to a GET request. Redaxo will find the
+         * updated module output of the article slice to display in the user interface
+         * and we are not bothered with wrong POST requests.
+         * All other POST requests are handled as intended by the route configuration.
+         */
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_GET['page'] === 'content/edit' && rex::isBackend()) {
+            $uri = str_replace('&function=edit', '', $_SERVER['HTTP_REFERER']);
+            header('Location: ' . $uri, true, 302);
+            exit;
+        }
+
         return require __DIR__ . '/../pages/frontend.php';
     }
 
